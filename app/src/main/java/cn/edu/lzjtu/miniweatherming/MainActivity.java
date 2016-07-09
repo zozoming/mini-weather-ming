bb91395c27cb2854cd50ed29b5863bdb656cfe60
zhangming <1044912118@qq.com>
2016-07-09 22:58:50 +0800

    first commit

---
+++ app/src/main/java/cn/edu/lzjtu/miniweatherming/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.edu.lzjtu.miniweatherming;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zzp on 2016/7/9.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
